f72dc086ad484d4f7e77799987f78aa5c4c60752
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-01-12 22:50:01 -0600

    Control de objeto
    Se realizo un script donde se coloco un vehículo para que el jugador conduzca y un obstáculo, para que golpee o evite.

---
+++ carController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroController : MonoBehaviour
+{
+    public float velocidad;//Velocidad de movimientos
+
+    private float forward;
+    private float right;//Movimiento
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Recuperar si presiono flecha arriba
+        forward = Input.GetAxis("Vertical");
+        //Recuperar si se presionó flecha der-izq
+        right = Input.GetAxis("Horizontal");
+        //Metodo para mover un GameObject (Acelerar - reversa)
+        //direccion * velocidad * tiempo
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime * forward);
+        //Metodo para mover un GameObject (Derecha - izquierda)
+        transform.Translate(Vector3.right * velocidad * Time.deltaTime * right);
+        
+    }
+}
